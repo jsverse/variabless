6812a785cb207eca9180e52d35801e2271f63390
shaharkazaz <shahar.kazaz@gmail.com>
2020-11-22 10:54:23 +0200

    style: 💄 space code

--- src/generateCSS.ts
+++ src/generateCSS.ts
@@ -1,4 +1,4 @@
-import postCss from 'postcss';
+import { Root } from 'postcss';
 import Rule from 'postcss/lib/rule';
 
 import { SelectorDefinition, VariableDefinition } from './types';
@@ -6,13 +6,16 @@ import { SelectorDefinition, VariableDefinition } from './types';
 export function generateCSS(
   containers: Record<string, { selectors: SelectorDefinition[]; variables: VariableDefinition[] }>
 ) {
-  const root = postCss.parse('');
+  const root = new Root();
   for (const [key, { variables, selectors }] of Object.entries(containers)) {
     const rule = new Rule({ selector: key });
+
     for (const variable of variables) {
       rule.append(variable);
     }
+
     root.append(rule);
+
     for (const { selector, ...node } of selectors) {
       const selectorRule = new Rule({ selector }).append(node);
       root.append(selectorRule);
--- src/validateRule.ts
+++ src/validateRule.ts
@@ -9,13 +9,16 @@ export function validateRule({ ruleKey, ruleSetKey, variableName, value, propert
   if (!variableName) {
     throw `Missing variable name for: ${ruleSetKey} -> ${ruleKey}`;
   }
+
   if (property && !selector) {
     throw `You must provide a selector when using properties!`;
   }
+
   if (!isString(value) && property) {
     if (isString(variableName) && !isTokenizedString(variableName)) {
       throw `Variable name must be a function or a tokenized string!`;
     }
+
     if (isString(selector) && !isTokenizedString(selector)) {
       throw `Selector must be a function or a tokenized string!`;
     }
